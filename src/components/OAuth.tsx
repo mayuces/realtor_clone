573d3424e890ddf85054dd1dc5d01b0a60b1eae7
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-03 18:15:17 +0300

    complete the Google Oauth functunality

--- src/components/OAuth.tsx
+++ src/components/OAuth.tsx
@@ -1,29 +1,66 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc';
+import 'firebase/auth';
+import { toast } from 'react-toastify';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router';
 
 
 export default function OAuth() {
+  const navigate = useNavigate();
+
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check for the user
+
+      const docRef = doc(db, 'users', user.uid);
+
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate('/');
+      
+    } catch (error) {
+      toast.error('Could not authorize with Google');
+    }
+  };
+
   return (
     <button
-    type='submit'
-    className='flex
-    items-center
-    justify-center
-    w-full
-    bg-red-600
-    text-white px-7
-    py-3
-    text-sm
-    font-medium
-    uppercase
-    rounded
-    shadow-md
-    hover:bg-red-700
-    transition duration-150
-    ease-in-out
-    hover:shadow-lg
-    active:bg-red-800'
-  >
+      type='button'
+      onClick={onGoogleClick}
+      className='flex
+      items-center
+      justify-center
+      w-full
+      bg-red-600
+      text-white px-7
+      py-3
+      text-sm
+      font-medium
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-red-700
+      transition duration-150
+      ease-in-out
+      hover:shadow-lg
+      active:bg-red-800'
+    >
     <FcGoogle 
       className='text-2xl bg-white rounded-full mr-2'
       />
