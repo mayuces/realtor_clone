aca5274628f20483581a17273c7ee49fdc4b1872
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-05 21:53:10 +0300

    add slider component to the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile.jsx';
