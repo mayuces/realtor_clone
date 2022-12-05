705a75832aba6bd7f04f9f584bff84e9ee6d31e4
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-05 18:49:53 +0300

    added share functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,11 +11,14 @@ import SwiperCore, {
   Pagination
 } from 'swiper';
 import 'swiper/css/bundle';
+import { FaShare } from 'react-icons/fa'
 
 
 export default function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
   const params = useParams();
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -59,6 +62,50 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div 
+        className='fixed
+          top-[13%]
+          right-[3%]
+          z-10
+          bg-white
+          cursor-pointer
+          border-2
+          border-gray-400
+          rounded-full
+          w-12
+          h-12
+          flex
+          justify-center
+          items-center'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare 
+          className='text-lg text-slate-500'
+        />
+      </div>
+      {shareLinkCopied &&
+        <p className='fixed
+          top-[23%]
+          right-[5%]
+          bg-white
+          border-2
+          border-gray-400
+          rounded-md
+          z-10
+          p-2
+          flex
+          justify-center
+          items-center'
+        >
+          Link Copied
+        </p>
+      }
     </main>
   )
 }
